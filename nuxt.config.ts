8be4c9af5772fe9b225ee32c7b2034c64450ad7a
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2026-08-14 10:41:37 +0800

    style: set global typography font family to Inter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   
+  app: {
+    head: {
+      title: 'Meet Ally - Credit Remedi Engine',
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap' }
+      ]
+    }
+  },
+  
   css: [
     '~/assets/css/main.css',
     'primeicons/primeicons.css'
